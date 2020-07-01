8b09db8add2e186e433a8e7a9063baef25d24869
Sripathi Krishnan <sripathi.krishnan@hashedin.com>
2020-07-01 11:51:57 +0530

    Adding support for charchabot within private teams

--- charcha/teams/views.py
+++ charcha/teams/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from charcha.discussions.models import update_gchat_space
 from .models import Team
 from .bot import members
+from django.http import HttpResponse, JsonResponse
 
 # This function does not depend on request / response objects
 # Ideally, it should have been in models.py
@@ -12,6 +13,13 @@ from .bot import members
 def sync_team(space, room_name):
     team_members = members(space)
     team_members = [(g["member"]["name"], g["member"]["displayName"]) for g in team_members]
+    
+    # Private direct messages between 3 or more people is an anonymous room without a name
+    # In that case, we create the room name from the names of the members
+    if not room_name:
+        room_name = ", ".join([t[1] for t in team_members])
+        room_name = room_name[:95]
+    
     team = Team.objects.upsert(space, room_name)
     team.sync_team_members(team_members)
 
@@ -36,7 +44,8 @@ def google_chatbot(request):
                 """
         elif event['space']['type'] == 'ROOM':
             space = event['space']['name']
-            room_name = event['space']['displayName']
+            # room name can be None for DMs between multiple people
+            room_name = event['space'].get('displayName', None)
             sync_team(space, room_name)
             text = "Done! I have created a new team in charcha. \
                 You can now have private discussions in charcha, and only members of this room will be able to participate."
